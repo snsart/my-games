ca5189b711bdacc78e67934ba9b80b38cd918b1e
snsart <snsart@163.com>
2019-06-14 18:08:28 +0800

    add

--- RayLine/src/ObjectDecorator.ts
+++ RayLine/src/ObjectDecorator.ts
@@ -1,6 +1,9 @@
 class ObjectDecorator extends egret.DisplayObject {
 
 	private static obj:egret.DisplayObject;
+	private static touchDown:boolean=false;
+	private static touchEnd:boolean=false;
+	private static touchMove:boolean=false;
 
 	public constructor() {
 		super();
@@ -8,7 +11,22 @@ class ObjectDecorator extends egret.DisplayObject {
 
 	public static get(obj:egret.DisplayObject):ObjectDecorator{
 		ObjectDecorator.obj=obj;
-		return ObjectDecorator;
+		return null;
+	}
+
+	public static addDragAction(stage:egret.Stage):ObjectDecorator{
+		ObjectDecorator.obj.addEventListener(egret.TouchEvent.TOUCH_BEGIN,ObjectDecorator.touchBeginHandler,this);
+		stage.addEventListener(egret.TouchEvent.TOUCH_MOVE,ObjectDecorator.stageTouchMoveHandler,this);
+		return null;
+	}
+
+	private static touchBeginHandler():void{
+		ObjectDecorator.touchDown=true;
+	}
+
+	private static stageTouchMoveHandler():void{
+		if(ObjectDecorator)
+
 	}
 
 	
